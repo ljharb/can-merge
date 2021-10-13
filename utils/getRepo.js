6061490613d06046898d096a6e160bb3dd92bab0
Jordan Harband <ljharb@gmail.com>
2021-10-13 00:12:25 -0700

    [eslint] fix linting errors

--- utils/getRepo.js
+++ utils/getRepo.js
@@ -4,6 +4,6 @@ const { execSync } = require('child_process');
 
 module.exports = function getRepo() {
 	const getRepos = String(execSync('git ls-remote --get-url'));
-	const pushRepoRegex = /(?<=github\.com(\/|:))(.*)(?=\.git*)/gm;
+	const pushRepoRegex = /(?<=github\.com(?:\/|:))(?:.*)(?=\.git*)/gm;
 	return getRepos.match(pushRepoRegex)[0];
 };
--- utils/getSHA.js
+++ utils/getSHA.js
@@ -3,5 +3,5 @@
 const { execSync } = require('child_process');
 
 module.exports = function getSHA() {
-    return String(execSync('git rev-parse --short HEAD')).trim();
+	return String(execSync('git rev-parse --short HEAD')).trim();
 };
