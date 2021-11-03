9745b49f53270d275f359fa884e5a48401777abe
Green-Ranger11 <junior.eteuati@gmail.com>
2021-11-03 16:26:31 +1300

    Add pr enum defining various cases

---
+++ utils/models/pullRequestStatus.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+	CLOSED: '⚠ This PR is closed',
+	CONFLICT: '⚠ There are merge conflicts with the base branch',
+	MERGEABLE: '✔ This PR is mergeable',
+	MERGED: 'ℹ This PR is already merged',
+	REVIEW_APPROVED: '✔ This PR has been approved',
+	REVIEW_DISAPPROVED: 'ℹ There are requested changes on this PR - merging is blocked',
+	REVIEW_REQUIRED: 'ℹ This PR requires a review - merging is blocked',
+	STATUS_FAILURE: '⚠ Some status checks are not successful',
+	STATUS_PENDING: 'ℹ Some status checks are pending',
+};
