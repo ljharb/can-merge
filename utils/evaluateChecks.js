6d01272f8d63d17765b185df9db02cc27fc29e32
Green-Ranger11 <junior.eteuati@gmail.com>
2021-11-08 11:44:02 +1400

    [Refactor] log checks in bin/can-merge

--- utils/evaluateChecks.js
+++ utils/evaluateChecks.js
@@ -2,8 +2,6 @@
 
 'use strict';
 
-const chalk = require('chalk');
-
 const parseChecks = (statusCheckRollup, name) => {
 	let pending = [];
 	let failure = [];
@@ -27,32 +25,20 @@ const parseChecks = (statusCheckRollup, name) => {
 	return { failure, pending };
 };
 
-module.exports = function evaluateChecks(response) {
-	const { repository: { pullRequest: { commits: { nodes: [{ commit: { statusCheckRollup } }] } } } } = response;
+module.exports = function evaluateChecks(pullRequest) {
+	const { commits: { nodes: [{ commit: { statusCheckRollup } }] } } = pullRequest;
 
 	for (const ctx of statusCheckRollup.contexts.nodes) {
 		if (ctx.__typename === 'StatusContext' && ctx.state !== 'SUCCESS') {
 			const { failure, pending } = parseChecks(statusCheckRollup, 'statusCheck');
-			if (pending.length) {
-				console.info(chalk.yellowBright(`Pending Checks (${pending.length}): ${pending.join(', ')}`));
-			}
-			if (failure.length) {
-				console.info(chalk.redBright(`Failed Checks (${failure.length}): ${failure.join(', ')}`));
-			}
-			return false;
+			return { failure, pending };
 		} else if (
 			ctx.__typename === 'CheckRun' && ctx.conclusion !== 'SUCCESS'
 		) {
 			const { failure, pending } = parseChecks(statusCheckRollup, 'checkRun');
-			if (pending.length > 0) {
-				console.info(chalk.yellowBright(`Pending Checks (${pending.length}): ${pending.join(', ')}`));
-			}
-			if (failure.length > 0) {
-				console.info(chalk.redBright(`Failed Checks (${failure.length}): ${failure.join(', ')}`));
-			}
-			return false;
+			return { failure, pending };
 		}
 	}
 
-	return true;
+	return { failure: [], pending: [] };
 };
