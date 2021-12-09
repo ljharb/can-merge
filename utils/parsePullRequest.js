7115197d0580a9ee3761b1c7f75c728ac06c02df
Jordan Harband <ljharb@gmail.com>
2021-12-09 10:09:22 -0800

    [Dev Deps] update `eslint`, `@ljharb/eslint-config`

--- utils/parsePullRequest.js
+++ utils/parsePullRequest.js
@@ -1,10 +1,9 @@
-/* eslint-disable max-len */
-/* eslint-disable operator-linebreak */
-
 'use strict';
 
 module.exports = function parsePullRequest(response) {
 	return response.pullRequest
 		? [response.pullRequest]
-		: response.commit?.associatedPullRequests.edges.flatMap(({ node }) => node) || response.pullRequests?.nodes || [];
+		: response.commit?.associatedPullRequests.edges.flatMap(({ node }) => node)
+			|| response.pullRequests?.nodes
+			|| [];
 };
