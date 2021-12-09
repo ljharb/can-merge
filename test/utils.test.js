eeb34138075dec5985aa97516a72cf71de273c5a
Green-Ranger11 <junior.eteuati@gmail.com>
2021-12-09 14:41:08 +1300

    [Test] Add tests for `getSHA`

--- test/utils.test.js
+++ test/utils.test.js
@@ -5,28 +5,39 @@ const test = require('tape');
 const evaluatePullRequest = require('../utils/evaluatePullRequest');
 const parsePullRequest = require('../utils/parsePullRequest');
 const getRepo = require('../utils/getRepo');
+const getSHA = require('../utils/getSHA');
 
 const { mockEvalPR, mockParsePR } = require('./mocks');
 
 test('evaluatePullRequest', (t) => {
-	t.plan(mockEvalPR.length);
 	mockEvalPR.forEach((mock) => {
 		t.equal(evaluatePullRequest(mock.res.repository.pullRequest), mock.expected, mock.description);
 	});
+
 	t.end();
 });
 
 test('parsePullRequest', (t) => {
-	t.plan(mockParsePR.length);
 	mockParsePR.forEach((mock) => {
 		t.deepEqual(parsePullRequest(mock.repository), mock.expected, mock.description);
 	});
+
 	t.end();
 });
 
 test('getRepo', (t) => {
-	t.plan(2);
 	t.match(getRepo(), /[^/]+\/can-merge/);
 	t.looseEqual(getRepo('invalidvalue'), null);
+
+	t.end();
+});
+
+test('getSHA', (t) => {
+	const long = getSHA();
+	const short = getSHA(true);
+	t.match(long, /^[a-zA-Z0-9]{40}$/);
+	t.match(short, /^[a-zA-Z0-9]{10}$/);
+	t.ok(long.startsWith(short), 'short SHA is a prefix of long SHA');
+
 	t.end();
 });
