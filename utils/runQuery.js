84bca415bef6284e94e6ad76f2f9a211ed2c4dfb
Jordan Harband <ljharb@gmail.com>
2021-12-24 13:26:05 -0800

    [Refactor] `runQuery`: convert args to object

--- utils/runQuery.js
+++ utils/runQuery.js
@@ -1,13 +1,10 @@
-/* eslint-disable object-curly-newline */
-/* eslint-disable max-params */
-
 'use strict';
 
 const { graphql } = require('@octokit/graphql');
 
 const buildQuery = require('./buildQuery');
 
-module.exports = async function runQuery(pr, repo, sha, token) {
+module.exports = async function runQuery({ repo, pr, sha, token }) {
 	const [owner, name] = repo.split('/');
 	const response = await graphql(buildQuery({ name, owner, pr, sha }), {
 		headers: {
