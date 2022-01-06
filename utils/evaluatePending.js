a7182a55489f703911eb5af784c6447218a33750
Green-Ranger11 <junior.eteuati@gmail.com>
2022-01-07 09:47:11 +1300

    [Fix] parsing response for `evaluatePending`

--- utils/evaluatePending.js
+++ utils/evaluatePending.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function evaluatePending(response) {
-	const { repository: { pullRequest: { commits: { nodes: [{ commit: { statusCheckRollup } }] } } } } = response;
+	const { search: { edges: [{ node: { commits: { nodes: [{ commit: { statusCheckRollup } }] } } }] } } = response;
 
 	// eslint-disable-next-line no-underscore-dangle
 	const pending = statusCheckRollup.contexts.nodes.filter((ctx) => (ctx.__typename === 'CheckRun' && ctx.status !== 'COMPLETED') || (ctx.__typename === 'StatusContext' && ctx.state === 'PENDING'));
