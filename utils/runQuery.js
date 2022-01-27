3a6fa74d6c38de7ef5c371c0c34310f1107d033d
Green-Ranger11 <junior.eteuati@gmail.com>
2022-01-28 01:44:45 +1300

    [New] catch errors from `runQuery`

--- utils/runQuery.js
+++ utils/runQuery.js
@@ -1,15 +1,19 @@
 'use strict';
 
 const { graphql } = require('@octokit/graphql');
-
 const buildQuery = require('./buildQuery');
 
 module.exports = async function runQuery({ repo, pr, sha, token }) {
 	const [owner, name] = repo.split('/');
-	const response = await graphql(buildQuery({ name, owner, pr, sha }), {
-		headers: {
-			authorization: `token ${token}`,
-		},
-	});
+	let response = null;
+	try {
+		response = await graphql(buildQuery({ name, owner, pr, sha }), {
+			headers: {
+				authorization: `token ${token}`,
+			},
+		});
+	} catch (err) {
+		throw err.message;
+	}
 	return response;
 };
