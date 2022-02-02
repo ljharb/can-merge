9be36d0130f843352bc45382091e8981a8fd957e
Jordan Harband <ljharb@gmail.com>
2022-02-02 12:19:03 -0800

    [Fix] `evaluatePending`: on a commit with no PR checks, `edges` is an empty array

--- utils/evaluatePending.js
+++ utils/evaluatePending.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function evaluatePending(response) {
-	const pullRequestPending = response.search?.edges[0].node.commits.nodes[0].commit.statusCheckRollup.state === 'PENDING';
+	const pullRequestPending = response.search?.edges[0]?.node.commits.nodes[0].commit.statusCheckRollup.state === 'PENDING';
 	const commitRequestPending = response.repository?.commit.statusCheckRollup.state === 'PENDING';
 
 	return pullRequestPending || commitRequestPending;
