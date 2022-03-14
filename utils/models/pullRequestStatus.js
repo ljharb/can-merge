76a5e61f09562fb6513147c21db6d12a5fc5be87
Green-Ranger11 <junior.eteuati@gmail.com>
2022-03-14 21:30:36 +1300

    [Refactor] rename statuses to required statuses

--- utils/models/pullRequestStatus.js
+++ utils/models/pullRequestStatus.js
@@ -8,7 +8,7 @@ module.exports = {
 	REVIEW_APPROVED: '✔ This PR has been approved',
 	REVIEW_DISAPPROVED: 'ℹ There are requested changes on this PR - merging is blocked',
 	REVIEW_REQUIRED: 'ℹ This PR requires a review - merging is blocked',
-	STATUS_FAILURE: '⚠ Some status checks are not successful',
-	STATUS_PENDING: 'ℹ Some status checks are pending',
+	STATUS_FAILURE: '⚠ Some required status checks are not successful',
+	STATUS_PENDING: 'ℹ Some required status checks are pending',
 	BYPASSABLE: 'ℹ This PR requires a review before merging, but as an ADMIN with merge rights you can bypass that restriction',
 };
