12d5d19f5ae8bb555526c1b30abbacb38bc4fb5d
PriyaBihani <priyabihani.official@gmail.com>
2022-03-17 09:15:18 +0530

    use synchronous child_process

--- utils/resolveIndentifier.js
+++ utils/resolveIndentifier.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const exec = require('child_process').execSync;
 
 // function to resolve the identifier to it's sha
-module.exports = async (identifier, short = false) => {
+module.exports = (identifier, short = false) => {
 	try {
-		const command = short ? `git show ${identifier} -s --format="%h"` : `git show ${identifier} -s --format="%H"`
-		const { stdout } = await exec(command);
+		const command = short ? `git show ${identifier} -s --format="%h"` : `git show ${identifier} -s --format="%H"`;
+		const  stdout  = exec(command).toString();
 		return stdout;
 	} catch (e) {
 		return null;
