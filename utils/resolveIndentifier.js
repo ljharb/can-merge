0cacae62aafe89669f507a391eaa43d5c63d0a79
Priya Bihani <priyabihani.official@gmail.com>
2022-03-17 09:23:38 +0530

    Update utils/resolveIndentifier.js
    Co-authored-by: Jordan Harband <ljharb@gmail.com>

--- utils/resolveIndentifier.js
+++ utils/resolveIndentifier.js
@@ -7,7 +7,7 @@ const exec = require('child_process').execSync;
 module.exports = (identifier, short = false) => {
 	try {
 		const command = short ? `git show ${identifier} -s --format="%h"` : `git show ${identifier} -s --format="%H"`;
-		const  stdout  = exec(command).toString();
+		const stdout = String(exec(command));
 		return stdout;
 	} catch (e) {
 		return null;
