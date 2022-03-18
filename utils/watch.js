e5374c191054f0182ca33e9ac9e385702c31d483
MaheraFurniturewala <maherafurniturewala1111@gmail.com>
2022-03-18 22:12:52 +0530

    Handle rate limit exceeded error
    Closes #71. Fixes #51.

--- utils/watch.js
+++ utils/watch.js
@@ -1,9 +1,27 @@
 'use strict';
 
+const chalk = require('chalk');
 const evaluatePending = require('../utils/evaluatePending');
+
+async function delay(ms) {
+	return new Promise((resolve) => {
+		setTimeout(resolve, ms);
+	});
+}
 
 module.exports = async function watch(retryDelay, getResponse) {
 	const response = await getResponse();
+
+	const { remaining: remAPIPoints, cost: lastReqCost, resetAt } = response.rateLimit;
+	const currentTime = new Date();
+	const resetTime = new Date(resetAt);
+	const milliSecondsLeft = resetTime - currentTime;
+	const secondsLeft = Math.ceil(milliSecondsLeft / 1000);
+
+	if ((remAPIPoints < lastReqCost) && (retryDelay < milliSecondsLeft)) {
+		console.error(chalk.grey(`API points exhausted. Command will run again in ${secondsLeft} seconds...`));
+		await delay(milliSecondsLeft);
+	}
 
 	const isPendingChecks = evaluatePending(response);
 
